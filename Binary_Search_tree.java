30447c10686ce235f7c2461fee7989132dbc14f6
Akshaj Juneja <43871535+akshaj1999@users.noreply.github.com>
2019-09-17 20:12:05 +0530

    BST (find,delete,insert,getsuccessor)

---
+++ Binary_Search_tree.java
@@ -0,0 +1,192 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.*;
+import java.math.*; 
+public class BinarySearchTree {
+    public static  Node root;
+    public BinarySearchTree(){
+        this.root = null;
+    }
+    
+    public boolean find(int id){
+        Node current = root;
+        while(current!=null){
+            if(current.data==id){
+                return true;
+            }else if(current.data>id){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
+    }
+    public boolean delete(int id){
+        Node parent = root;
+        Node current = root;
+        boolean isLeftChild = false;
+        while(current.data!=id){
+            parent = current;
+            if(current.data>id){
+                isLeftChild = true;
+                current = current.left;
+            }else{
+                isLeftChild = false;
+                current = current.right;
+            }
+            if(current ==null){
+                return false;
+            }
+        }
+        
+        if(current.left==null && current.right==null){
+            if(current==root){
+                root = null;
+            }
+            if(isLeftChild ==true){
+                parent.left = null;
+            }else{
+                parent.right = null;
+            }
+        }
+
+        else if(current.right==null){
+            if(current==root){
+                root = current.left;
+            }else if(isLeftChild){
+                parent.left = current.left;
+            }else{
+                parent.right = current.left;
+            }
+        }
+        else if(current.left==null){
+            if(current==root){
+                root = current.right;
+            }else if(isLeftChild){
+                parent.left = current.right;
+            }else{
+                parent.right = current.right;
+            }
+        }else if(current.left!=null && current.right!=null){
+            
+            Node successor   = getSuccessor(current);
+            if(current==root){
+                root = successor;
+            }else if(isLeftChild){
+                parent.left = successor;
+            }else{
+                parent.right = successor;
+            }           
+            successor.left = current.left;
+        }       
+        return true;        
+    }
+    
+    public Node getSuccessor(Node deleleNode){
+        Node successsor =null;
+        Node successsorParent =null;
+        Node current = deleleNode.right;
+        while(current!=null){
+            successsorParent = successsor;
+            successsor = current;
+            current = current.left;
+        }
+        if(successsor!=deleleNode.right){
+            successsorParent.left = successsor.right;
+            successsor.right = deleleNode.right;
+        }
+        return successsor;
+    }
+    public void insert(int id){
+        Node newNode = new Node(id);
+        if(root==null){
+            root = newNode;
+            return;
+        }
+        Node current = root;
+        Node parent = null;
+        while(true){
+            parent = current;
+            if(id<current.data){                
+                current = current.left;
+                if(current==null){
+                    parent.left = newNode;
+                    return;
+                }
+            }else{
+                current = current.right;
+                if(current==null){
+                    parent.right = newNode;
+                    return;
+                }
+            }
+        }
+    }
+}
+
+class Node{
+    int data;
+    Node left;
+    Node right; 
+    public Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+ class Demo{
+   
+    public static void main(String[] args) throws Exception {
+        FastScanner cin = new FastScanner(System.in);
+        StringBuffer ans = new StringBuffer();
+       int t = cin.nextInt();
+       while(t-->0)
+       {
+      
+      } 
+   }
+
+        
+    static class FastScanner {
+        private BufferedReader reader = null;
+        private StringTokenizer tokenizer = null;
+ 
+        public FastScanner(InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+ 
+            return tokenizer.nextToken("\n");
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+         
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+    }
+  }
